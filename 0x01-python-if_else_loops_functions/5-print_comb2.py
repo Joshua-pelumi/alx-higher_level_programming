f8169b102fcb1133076e2228ccddbc778a67e1c5
Joshua-pelumi <joshuaogundairo9@gmail.com>
2023-03-08 22:30:53 +0100

    add new files and fix bug

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
-for i in range(0, 100):
-    if i < 99:
-        print("{0:02}".format(i), end=', ')
-    else:
-        print(i)
+#!/usr/bin/python3
+for i in range(0, 99):
+    print("{0:02d}".format(i), end=", ")
+print("{:d}".format(99))
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 def uppercase(str):
-    for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            str[i] = chr(ord(str[i]) - 32)
-strg = 'i am'
-uppercase(strg)
-print(strg)
+    for i in str:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            i = chr(ord(i) - 32)
+    print("{:s}".format(i), end="")
+    print()
